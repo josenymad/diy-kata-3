77a0c60cb6fd1a5f63c383b7078243198098217a
Jose Wills <jose.wills3@gmail.com>
2023-03-07 22:20:45 +0000

    solved fizzbuzz

--- test/kata1.fizzBuzz.test.js
+++ test/kata1.fizzBuzz.test.js
@@ -1,11 +1,26 @@
 const { fizzBuzz } = require("../src");
 
 describe("fizzBuzz", () => {
-  test("returns Fizz when passed a multiple of 3", () => {});
+  it("returns Fizz when passed a multiple of 3", () => {
+    expect(fizzBuzz(3)).toEqual("Fizz");
+    expect(fizzBuzz(9)).toEqual("Fizz");
+  });
 
-  test("returns Buzz when passed a multiple of 5", () => {});
+  it("returns Buzz when passed a multiple of 5", () => {
+    expect(fizzBuzz(5)).toEqual("Buzz");
+    expect(fizzBuzz(25)).toEqual("Buzz");
+  });
 
-  test("returns FizzBuzz when passed a multiple 3 and 5", () => {});
+  it("returns FizzBuzz when passed a multiple 3 and 5", () => {
+    expect(fizzBuzz(15)).toEqual("FizzBuzz");
+    expect(fizzBuzz(30)).toEqual("FizzBuzz");
+    expect(fizzBuzz(60)).toEqual("FizzBuzz");
+    expect(fizzBuzz(90)).toEqual("FizzBuzz");
+  });
 
-  test("returns the number when it isn't a multiple of 3 or 5", () => {});
+  it("returns the number when it isn't a multiple of 3 or 5", () => {
+    expect(fizzBuzz(4)).toEqual(4);
+    expect(fizzBuzz(16)).toEqual(16);
+    expect(fizzBuzz(76)).toEqual(76);
+  });
 });
