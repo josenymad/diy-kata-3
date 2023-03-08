9da46165e5270fb34cf91d5796af255c0748c992
Jose Wills <jose.wills3@gmail.com>
2023-03-08 21:53:08 +0000

    solved booleanToWord

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,10 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+  it("returns Yes when passed true", () => {
+    expect(booleanToWord(true)).toEqual("Yes");
+  });
+  it("returns No when passed false", () => {
+    expect(booleanToWord(false)).toEqual("No");
+  });
 });
