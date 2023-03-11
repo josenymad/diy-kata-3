4004518cc7c474fdfa67750f9c6b6cc3d81bcdcc
Jose Wills <jose.wills3@gmail.com>
2023-03-11 08:05:51 +0000

    passed reachDestination

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,4 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) =>
+  `I should be there in ${Math.ceil((distance / speed) * 2) / 2} hours.`;
 
 module.exports = reachDestination;
