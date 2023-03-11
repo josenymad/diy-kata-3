2a01e032ad9975f9eaa904323562d5a7f7ca2f61
Jose Wills <jose.wills3@gmail.com>
2023-03-11 07:26:58 +0000

    passed humanCatDogYears

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,13 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (number) => {
+  if (number === 1) {
+    return [number, 15, 15];
+  }
+  if (number === 2) {
+    return [number, 24, 24];
+  }
+  if (number >= 3) {
+    return [number, 24 + (number - 2) * 4, 24 + (number - 2) * 5];
+  }
+};
 
 module.exports = humanCatDogYears;
