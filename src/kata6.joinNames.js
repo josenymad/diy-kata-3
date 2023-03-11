04e32c5e8514a620b8bcb769f56c5cd3efbff513
Jose Wills <jose.wills3@gmail.com>
2023-03-11 09:55:36 +0000

    passed joinNames

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,13 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+  const namesArray = namesObj.map((person) => person.name);
+
+  const stringOfNames = namesArray.reduce((acc, curr, index) => {
+    if (index === namesArray.length - 1) {
+      return acc + " & " + curr;
+    }
+    return acc + ", " + curr;
+  });
+  return stringOfNames;
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,41 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  it("returns string of names, seperated by commas and an ampersand", () => {
+    expect(
+      joinNames([
+        {
+          name: "Bart",
+        },
+        {
+          name: "Lisa",
+        },
+        {
+          name: "Maggie",
+        },
+      ])
+    ).toEqual("Bart, Lisa & Maggie");
+    expect(
+      joinNames([
+        {
+          name: "Chandler",
+        },
+        {
+          name: "Monica",
+        },
+        {
+          name: "Phoebe",
+        },
+        {
+          name: "Rachel",
+        },
+        {
+          name: "Ross",
+        },
+        {
+          name: "Joey",
+        },
+      ])
+    ).toEqual("Chandler, Monica, Phoebe, Rachel, Ross & Joey");
+  });
 });
